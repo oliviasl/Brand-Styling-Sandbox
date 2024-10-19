5a790db432664b8dd919f03d6855ef1239d557fe
oliviasl <oslai@usc.edu>
2024-10-19 14:01:21 -0700

    Update fonts

--- tweens/src/app/api/generate-design-tokens/prompt.ts
+++ tweens/src/app/api/generate-design-tokens/prompt.ts
@@ -6,6 +6,26 @@ Every field in the provided JSON schema is required; you must provide a complete
 
 For fonts, please be creative!
 Here are the fonts you may choose from:
+- Roboto (Sans-serif): Corporate, Universal, Minimal
+- Open Sans (Sans-serif): Friendly, Universal, Minimal
+- Inter (Sans-serif): Corporate, Universal, Minimal
+- Montserrat (Sans-serif): Corporate, Minimal, Universal
+- Lato (Sans-serif): Corporate, Friendly, Universal
+- Poppins (Sans-serif): Friendly, Universal, Fun
+- Source Sans Pro (Sans-serif): Corporate, Universal, Friendly
+- Merriweather (Serif): Corporate, Universal, Friendly
+- Nunito (Sans-serif): Friendly, Fun, Universal
+- Ubuntu (Sans-serif): Corporate, Friendly, Universal
+- Playfair Display (Serif): Corporate, Abstract, Universal
+- Rubik (Sans-serif): Friendly, Universal, Fun
+- DM Sans (Sans-serif): Corporate, Minimal, Friendly
+- Lora (Serif): Friendly, Universal, Minimal
+- Josefin Sans (Sans-serif): Abstract, Minimal, Fun
+- Overpass (Sans-serif): Corporate, Minimal, Abstract
+- Vollkorn (Serif): Friendly, Universal, Corporate
+- Alegreya (Serif): Friendly, Universal, Fun
+- Spectral (Serif): Corporate, Minimal, Abstract
+- Cormorant (Serif): Abstract, Minimal, Friendly
 - Prociono (Serif): Abstract, Friendly
 - Ranchers (Sans-serif): Friendly, Fun
 - Englebert (Sans-serif): Friendly, Fun
