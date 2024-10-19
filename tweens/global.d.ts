8685310f5d33c1e84be929e52bc50d3a1b3cdc21
oliviasl <oslai@usc.edu>
2024-10-19 14:34:35 -0700

    global.d.ts update

--- tweens/global.d.ts
+++ tweens/global.d.ts
@@ -1,1 +1,11 @@
-declare module "*";
+// global.d.ts or tailwind.d.ts
+declare global {
+    interface Window {
+      tailwind?: any; // You can specify a more detailed type if you know it
+      theme?: any;
+      
+    }
+  }
+  
+  export {};
+  
